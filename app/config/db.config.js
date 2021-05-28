9d71cce139d1a127aacf9dfd92ff035b57cf4079
Shahnaz Shaik <82141569+shahnaz12-dev@users.noreply.github.com>
2021-05-28 11:08:39 +0530

    developed auth apis

---
+++ app/config/db.config.js
@@ -0,0 +1,14 @@
+// Note : this is the Postgres DB configuration
+module.exports = {
+  HOST: "localhost",
+  USER: "postgres",
+  PASSWORD: "Mohib123@",
+  DB: "ETrackerDB",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
