fba6a7d6cd5c86375f838ed52e6e6ff47fac4232
Shahnaz Shaik <82141569+shahnaz12-dev@users.noreply.github.com>
2021-06-04 15:25:22 +0530

    changed the db sync

--- server.js
+++ server.js
@@ -17,28 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = require("./app/models");
 const Role = db.role;
-
-db.sequelize.sync({ force: true }).then(() => {
-  console.log("Drop and Resync Db");
-  initial();
-});
-
-function initial() {
-  Role.create({
-    id: 1,
-    name: "user",
-  });
-
-  Role.create({
-    id: 2,
-    name: "moderator",
-  });
-
-  Role.create({
-    id: 3,
-    name: "admin",
-  });
-}
+db.sequelize.sync();
 
 require("./app/routes/auth.routes")(app);
 require("./app/routes/user.routes")(app);
